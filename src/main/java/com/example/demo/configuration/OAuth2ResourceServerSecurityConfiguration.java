ee10ad96fa5744dd75c86c4ec02e6ee03794b10a
krishnaprasadreddypundru <krishna.work1281@gmail.com>
2024-02-03 13:41:56 -0500

    H2 is now accessing without oauth 2

--- src/main/java/com/example/demo/configuration/OAuth2ResourceServerSecurityConfiguration.java
+++ src/main/java/com/example/demo/configuration/OAuth2ResourceServerSecurityConfiguration.java
@@ -6,9 +6,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -25,8 +27,9 @@ public class OAuth2ResourceServerSecurityConfiguration {
                 .authorizeHttpRequests((authorize) -> authorize
                         .requestMatchers(HttpMethod.GET, "/message/**").hasAuthority("SCOPE_message:read")
                         .requestMatchers(HttpMethod.POST, "/message/**").hasAuthority("SCOPE_message:write")
-                        .anyRequest().authenticated()
-                )
+                        .requestMatchers("/h2-console/**").permitAll())
+                .headers(AbstractHttpConfigurer::disable)
+                .csrf(csrf -> csrf.ignoringRequestMatchers(AntPathRequestMatcher.antMatcher("/h2-console/**")))
                 .oauth2ResourceServer((oauth2) -> oauth2.jwt(withDefaults()));
         return http.build();
     }
